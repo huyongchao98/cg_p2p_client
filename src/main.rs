bd421ff0ea43880c180eb49744a96f471f3fb453
huych <>
2023-09-09 18:06:30 +0800

    feat: add screen shot

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
  * @Author: huych 305856242@qq.com
  * @Date: 2023-09-03 18:34:58
  * @LastEditors: huych
- * @LastEditTime: 2023-09-09 17:03:19
+ * @LastEditTime: 2023-09-09 18:03:45
  * @FilePath: \cg_p2p_client (copy)\src\main.rs
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
